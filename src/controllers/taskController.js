def35346fece059188dcfc924bfb5fe78fb1069a
Elidad <etundula@charlotte.edu>
2025-10-28 22:18:09 -0400

    Changes for lab

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -10,3 +10,29 @@ export async function createTask(req, res, next) {
   const task = await taskService.createTask({ title, completed });
   res.status(201).json(task);
 }
+
+export async function getTaskById(req, res, next) {
+  try {
+    const { id } = req.params;
+    const numId = Number(id);
+
+    // 400 invalid id
+    if (!Number.isInteger(numId)) {
+      return res.status(400).json({
+        error: 'Validation failed',
+        details: ['ID must be a number'],
+      });
+    }
+
+    const task = await taskService.getTaskById(numId);
+
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' });
+    }
+
+    // 200 ok
+    return res.json(task);
+  } catch (err) {
+    next(err);
+  }
+}
